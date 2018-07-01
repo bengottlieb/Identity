f2e9d33d953e032fec30808b2942f948086bfa64
Ben Gottlieb <ben@standalone.com>
2018-07-01 10:08:22 -0500

    initial code commit

---
+++ Identity/Framework Code/Enums.swift
@@ -0,0 +1,11 @@
+//
+//  Enums.swift
+//  Identity_iOS
+//
+//  Created by Ben Gottlieb on 7/1/18.
+//  Copyright © 2018 Stand Alone, inc. All rights reserved.
+//
+
+import Foundation
+
+public enum Provider { case twitter, facebook, gamecenter, icloud, google, device }
---
+++ IdentityTesting/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IdentityTesting
+//
+//  Created by Ben Gottlieb on 7/1/18.
+//  Copyright © 2018 Stand Alone, inc. All rights reserved.
+//
+
+import UIKit
+import Identity
+
+class ViewController: UIViewController {
+	let kind = Identity.Provider.gamecenter
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
