5804bbc68f44aad545080b8b48baad75e6ba7eed
Ben Gottlieb <ben@standalone.com>
2018-10-29 05:44:56 -0500

    use the Info.plist to extract a FacebookAppID

--- Identity/Framework Code/Facebook.swift
+++ Identity/Framework Code/Facebook.swift
@@ -14,7 +14,7 @@ public class Facebook: Service {
 	public static let instance = Facebook()
 	var completions: [LoginCompletion] = []
 	override var provider: Provider { return .facebook }
-	public var applicationID: String? { didSet { if let appID = self.applicationID { FBSDKSettings.setAppID(appID) }}}
+	public var applicationID: String? { return Bundle.main.infoDictionary?["FacebookAppID"] as? String }
 	
 	public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) {
 		FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
